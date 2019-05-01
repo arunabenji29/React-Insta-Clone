09352bd63bbb98e07c76598f349e9b949e85baa6
Aruna Benjamin <arunabenji29@gmail.com>
2019-05-01 13:33:45 -0600

    Day 2 task search working

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -6,87 +6,92 @@ import './App.css';
 
 class App extends React.Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      data:[],
-      searchResult:[],
-      searchField:''
+      data: [],
+      searchResult: [],
+      searchField: ''
     }
     console.log('App:constructor running')
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log('App:CDM running')
     this.setState({
-      data:dummyData,
-      searchResult:[],
-      searchField:''
+      data: dummyData,
+      searchResult: [],
+      searchField: ''
     })
-    
-    
+
+
   }
 
- searchHandle = (event) => {
-    console.log('search content '+event.target.value)
+  searchHandle = (event) => {
+    console.log('search content ' + event.target.value)
     this.setState({
-        searchField: event.target.value
+      searchField: event.target.value,
+      searchResult: [],
     })
     console.log('search handle ' + this.state.searchField)
-}
-
-
-searchSubmit = (event) => {
-  event.preventDefault();
-
-  let matched = this.state.data.filter(post => post.username.includes(this.state.searchField))
-  
-  this.setState({
-      searchResult: matched
-  })
-
-  console.log('search result ' + this.state.searchResult)
-
-}
-
- render(){
-    console.log('App:rendering')
-    if(this.state.searchResult.length === 0) {
-    return (
-      <div className="App">
-       
-        <SearchBar userProp={this.state}/>
-        
-        
-        {this.state.data.map((data,index) => (
-        
-          <PostContainer key={index} dataProp={data}/> 
-        ))}
-        
-      </div>
-    );
-        }
-
-        else{
-          return (
-            <div className="App">
-             
-              <SearchBar 
-              searchHandleProp={this.searchHandle} 
-              searchSubmitProp={this.searchSubmit} 
-              fieldProp={this.state.searchField}/>
-              
-              
-              {this.state.searchResult.map(data => (
-              
-                <PostContainer dataProp={data}/> 
-              ))}
-              
-            </div>
-          );
-        }
   }
-  
+
+
+  searchSubmit = (event) => {
+    event.preventDefault();
+
+    let matched = this.state.data.filter(post => post.username.includes(this.state.searchField))
+
+    this.setState({
+      searchResult: matched
+    })
+
+    console.log('search result ' + this.state.searchResult)
+
+  }
+
+  render() {
+    console.log('App:rendering')
+    if (this.state.searchResult.length === 0) {
+      return (
+        <div className="App">
+
+          <SearchBar
+            userProp={this.state.searchField}
+            searchSubmitProp={this.searchSubmit}
+            searchHandleProp={this.searchHandle}
+          />
+
+
+          {this.state.data.map((data, index) => (
+
+            <PostContainer key={index} dataProp={data} />
+          ))}
+
+        </div>
+      );
+    }
+
+    else {
+      return (
+        <div className="App">
+
+          <SearchBar
+            searchHandleProp={this.searchHandle}
+            searchSubmitProp={this.searchSubmit}
+            fieldProp={this.state.searchField} />
+
+
+          {this.state.searchResult.map(data => (
+
+            <PostContainer dataProp={data} />
+          ))}
+
+        </div>
+      );
+    }
+  }
+
 }
 
 export default App;
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,51 +1,90 @@
 import React from 'react'
 import './searchBar.css'
 
-class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            
-        }
-    }
 
+const SearchBar = (props) => {
 
-    render() {
+    console.log(props.userProp)
         return (
             <div className='search-bar'>
                 <div className='left'>
                     <div className='logo'>
-
+                        
                         <i className="fab fa-instagram"></i>
                     </div>
                     <p className='logo-text'><strong>Instagram</strong></p>
                 </div>
-
-
-                <form onSubmit={this.props.searchSubmitProp}>
-                    <input
+    
+               <form onSubmit={props.searchSubmitProp}>
+                     <input
                         placeholder='&#128269;Search'
-                        value={this.props.fieldProp}
-                        onChange={this.props.searchHandleProp} />
+                        value={props.userProp}
+                        onChange={props.searchHandleProp} />
                 </form>
-
+    
                 <div className='right'>
                     <div className='compass'>
                         <i className="far fa-compass"></i>
-
+                        
                     </div>
                     <div className='heart'>
-                        <i className="far fa-heart" ></i>
-
+                    <i className="far fa-heart"></i>
+                        
                     </div>
                     <div className='person'>
-                        <i className="far fa-user"></i>
-
+                    <i className="far fa-user"></i>
+                        
                     </div>
-                </div>
+                </div> 
             </div>
         );
     }
-}
+
+// class SearchBar extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state={
+
+//         }
+//     }
+
+
+//     render() {
+//         return (
+//             <div className='search-bar'>
+//                 <div className='left'>
+//                     <div className='logo'>
+
+//                         <i className="fab fa-instagram"></i>
+//                     </div>
+//                     <p className='logo-text'><strong>Instagram</strong></p>
+//                 </div>
+
+
+//                 <form onSubmit={this.props.searchSubmitProp}>
+//                     <input
+//                         placeholder='&#128269;Search'
+//                         value={this.props.fieldProp}
+//                         onChange={this.props.searchHandleProp} />
+//                 </form>
+
+//                 <div className='right'>
+//                     <div className='compass'>
+//                         <i className="far fa-compass"></i>
+
+//                     </div>
+//                     <div className='heart'>
+//                         <i className="far fa-heart" ></i>
+
+//                     </div>
+//                     <div className='person'>
+//                         <i className="far fa-user"></i>
+
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 export default SearchBar
